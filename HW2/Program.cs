6b6ff429c6a199acf9827fb3cbf41de24439fbc6
smidy2002 <59875002+smidy2002@users.noreply.github.com>
2024-11-21 12:27:23 +0300

    Залил вторую домашку

---
+++ HW2/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Text;
+
+namespace MyApp 
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            User TheBoyWhoLived = new();
+
+            TheBoyWhoLived.userinit(57, "Harry", "Potter");
+
+            Console.WriteLine(TheBoyWhoLived.GetFullNameAndId());
+        }
+        class User
+        {
+            public int id = -1;
+            string name = "Undefined";
+            string surname = "Undefined";
+
+            public void userinit(int id,string name,string surname)
+            {
+                this.id = id;
+                this.name = name;
+                this.surname = surname;
+            }
+
+            public string GetFullNameAndId()
+            {
+                var nameid = new StringBuilder();
+
+                nameid.Append(name);
+                nameid.Append(" ");
+                nameid.Append(surname);
+                nameid.Append(" c ID=");
+                nameid.Append(id);
+
+                return nameid.ToString();
+            }
+        }
+    }
+}
